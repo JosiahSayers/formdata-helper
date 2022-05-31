2453e988e49221107979cc0ebacc11bf6264a96d
Josiah Sayers <jsayers@covermymeds.com>
2022-05-31 19:59:34 -0400

    add test for blob in formdata

--- packages/base/src/parse.test.ts
+++ packages/base/src/parse.test.ts
@@ -12,4 +12,19 @@ describe('parseForm', () => {
       multiple: ['hello1', 'hello2'],
     });
   });
+
+  it('handles blobs', async () => {
+    const formWithFile = new FormData();
+    formWithFile.append('blob', new Blob(['blob', 'text']));
+    formWithFile.append('multi-blob', new Blob(['first']));
+    formWithFile.append('multi-blob', new Blob(['second']));
+    const output = parseForm<{ blob: Blob, 'multi-blob': Blob[] }>(formWithFile);
+
+    expect(output.blob).toBeDefined();
+    expect(output.blob instanceof Blob).toBe(true);
+    expect(await output.blob.text()).toBe('blobtext');
+    expect(output["multi-blob"].length).toBe(2);
+    expect(await output["multi-blob"][0].text()).toBe('first');
+    expect(await output["multi-blob"][1].text()).toBe('second');
+  });
 });
